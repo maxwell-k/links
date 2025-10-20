67752e50556f5685e00df1f31c6d07b18cd3ec40
Keith Maxwell <keith.maxwell@gmail.com>
2025-10-20 16:12:42 +0100

    Minify CSS

--- css2/main.ts
+++ css2/main.ts
@@ -18,6 +18,7 @@ async function main() {
   const { code: transformed } = transform({
     filename: "input.css",
     code: new TextEncoder().encode(purged),
+    minify: true,
     visitor: {
       Declaration: {
         custom: { float },
