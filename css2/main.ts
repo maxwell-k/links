99b610f18061cff81339a788cf2a46f8315575f9
Keith Maxwell <keith.maxwell@gmail.com>
2025-10-24 10:35:33 +0100

    Further optimise CSS

--- css2/main.ts
+++ css2/main.ts
@@ -13,6 +13,9 @@ async function main() {
   const [{ css: purged }] = await new PurgeCSS().purge({
     content: ["../index.html"],
     css: [{ raw: new TextDecoder().decode(bundled) }],
+    fontFace: true,
+    keyframes: true,
+    variables: true,
   });
 
   const { code: readable } = transform({
