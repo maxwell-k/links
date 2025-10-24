d26b2691120d7e69d226bfb4f6934d491037171e
Keith Maxwell <keith.maxwell@gmail.com>
2025-10-24 12:19:34 +0100

    Remove unused font faces

--- css2/main.ts
+++ css2/main.ts
@@ -2,7 +2,9 @@ import fs from "node:fs";
 import { bundle, transform } from "lightningcss";
 import { PurgeCSS } from "purgecss";
 
-import type { CustomProperty, Declaration } from "lightningcss";
+import type { CustomProperty, Declaration, FontFaceRule } from "lightningcss";
+
+const ignored_font_faces = { family: "Open Sans", weights: [600, 800] };
 
 async function main() {
   const { code: bundled } = bundle({
@@ -22,6 +24,9 @@ async function main() {
     code: new TextEncoder().encode(purged),
     minify: false,
     visitor: {
+      Rule: {
+        "font-face": (rule) => font(rule.value),
+      },
       Declaration: {
         custom,
         position,
@@ -57,6 +62,19 @@ const position = (declaration: Declaration) => {
     declaration.value.value[0].value.type === "ident" &&
     declaration.value.value[0].value.value === "center"
   ) return [];
+};
+
+const font = (rule: FontFaceRule) => {
+  const properties = rule.properties;
+  const family = properties.find((i) => i.type === "font-family");
+  if (!family?.value.includes(ignored_font_faces.family)) return;
+  const weight = properties
+    .find((i) => i.type === "font-weight")
+    ?.value
+    .find((i) => i.type === "absolute")
+    ?.value;
+  if (weight?.type !== "weight") return;
+  if (ignored_font_faces.weights.includes(weight.value)) return [];
 };
 
 await main();
